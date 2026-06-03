a75e9874a46f98fe593db613d34b67b67b529002
Jhen2490 <franzvincent@gmail.com>
2026-06-03 18:18:04 +0800

    learning lesson two

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 import express from 'express';
+import cors from 'cors';
+
+import type { Express, Request, Response } from 'express';
+
+import { Pets } from "./data/pets"
 
 const PORT = 8000;
-const app = express();
+const app: Express = express();
+
+
+
+app.get( '/', (req: Request, res: Response) => {
+    res.json(Pets)
+})
 
 app.listen( PORT, () => {
     console.log(`Listening on port: ${PORT}`)
+})
+
+app.use((req: Request, res: Response) : void => {
+    res.status(404).json({ message: "Endpoint not found. "})
 })
